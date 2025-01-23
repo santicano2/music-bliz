d5b3c2d1aa06353ea179dda29138c48c7e989983
Santi <canosantiago404@gmail.com>
2025-01-23 11:09:05 -0300

    feat: added lucide-react and deleted some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import "./App.css";
-
 function App() {
-  return (
-    <div>
-      <h1 className="text-3xl text-green-500">Hola</h1>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
